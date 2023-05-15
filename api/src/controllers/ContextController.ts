cc31d7c5071dea059b826a8b07984a890bc1eb70
Denni Wernersson <dw222fy@student.lnu.se>
2023-05-15 13:08:54 +0200

    Add data endpoint to see workfiles

--- api/src/controllers/ContextController.ts
+++ api/src/controllers/ContextController.ts
@@ -22,11 +22,22 @@ interface ContextResponse {
 }
 
 export default class ContextController {
-  async index (req: Request, res: Response, next: NextFunction) {
+  async index(req: Request, res: Response, next: NextFunction) {
     res.send('Index controller')
   }
 
-  async runScraper (req: Request, res: Response, next: NextFunction) {
+  async data(req: Request, res: Response, next: NextFunction) {
+    try {
+      const scrapedContent = JSON.parse(
+        fs.readFileSync('./workfiles/raw_info.json').toString()
+      )
+      res.status(200).send(scrapedContent.informationBlobs)
+    } catch (error) {
+      res.status(404).send([])
+    }
+  }
+
+  async runScraper(req: Request, res: Response, next: NextFunction) {
     try {
       const { pages } = req.body
 
@@ -47,21 +58,21 @@ export default class ContextController {
       python.on('close', (code) => {
         console.log(`child process exited with code ${code}`)
         logs.push(`child process exited with code ${code}`)
-
       })
       res.send({
         message: 'Scraper started successfully, scraping ' + pages + ' pages',
       })
-
     } catch (err: any) {
       next(createError(500, `SCRAPER_ERROR: ${err.message}`))
     }
   }
 
-  async uploadScrapedContent (req: Request, res: Response, next: NextFunction) {
+  async uploadScrapedContent(req: Request, res: Response, next: NextFunction) {
     try {
       // TODO: Make sure it works with larger batches of data.
-      const scrapedContent = JSON.parse(fs.readFileSync('./workfiles/raw_info.json').toString())
+      const scrapedContent = JSON.parse(
+        fs.readFileSync('./workfiles/raw_info.json').toString()
+      )
       const infoBlobs = scrapedContent.informationBlobs
       const pineconePrep: PineconePrep[] = []
 
@@ -69,57 +80,59 @@ export default class ContextController {
         const title: string = infoBlobs[i].title
         const text: string = infoBlobs[i].content
         const url: string = infoBlobs[i].url
-        const id: string = (url + title).replace(/\W+/g, "X")
+        const id: string = (url + title).replace(/\W+/g, 'X')
         let currentPrep: PineconePrep = {
           id: id,
           text: text,
           metadata: {
             source_id: id,
             url: url,
-            created_at: new Date()
-          }
+            created_at: new Date(),
+          },
         }
         pineconePrep.push(currentPrep)
       }
 
-      const retrievalUrl = "http://retrieval-plugin:8080"
+      const retrievalUrl = 'http://retrieval-plugin:8080'
 
       const payload = {
-        documents: pineconePrep
+        documents: pineconePrep,
       }
-      const response = await axios.post(retrievalUrl + "/upsert", payload, {
+      const response = await axios.post(retrievalUrl + '/upsert', payload, {
         headers: {
-          Authorization: 'Bearer ' + process.env.BEARER_TOKEN
-        }
+          Authorization: 'Bearer ' + process.env.BEARER_TOKEN,
+        },
       })
 
       res.send({
         pineconeResponse: response.status,
-        dataUploaded: pineconePrep
+        dataUploaded: pineconePrep,
       })
-
     } catch (err: any) {
       next(err)
     }
   }
 
-  async getContext (req: Request, res: Response, next: NextFunction) {
+  async getContext(req: Request, res: Response, next: NextFunction) {
     try {
-      const { currentQuery, maxTokens }: { currentQuery: string, maxTokens: number } = req.body
+      const {
+        currentQuery,
+        maxTokens,
+      }: { currentQuery: string; maxTokens: number } = req.body
       const chunkSize = 300
-      const retrievalUrl = "http://retrieval-plugin:8080"
+      const retrievalUrl = 'http://retrieval-plugin:8080'
       const payload = {
         queries: [
           {
             query: currentQuery,
-            top_k: Math.floor(maxTokens / chunkSize)
-          }
-        ]
+            top_k: Math.floor(maxTokens / chunkSize),
+          },
+        ],
       }
-      const response = await axios.post(retrievalUrl + "/query", payload, {
+      const response = await axios.post(retrievalUrl + '/query', payload, {
         headers: {
-          Authorization: "Bearer " + process.env.BEARER_TOKEN
-        }
+          Authorization: 'Bearer ' + process.env.BEARER_TOKEN,
+        },
       })
 
       const results = response.data.results[0]?.results
@@ -127,14 +140,13 @@ export default class ContextController {
       for (let i = 0; i < results.length; i++) {
         result.push({
           information: results[i].text,
-          url: results[i].metadata.url
+          url: results[i].metadata.url,
         })
       }
 
       res.send({
-        context: result
+        context: result,
       })
-
     } catch (err: unknown) {
       next(err)
     }
